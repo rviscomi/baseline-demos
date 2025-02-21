42bdb1d8ded91ccfbeefc9c67b019fead5372a3c
Rick Viscomi <rviscomi@google.com>
2025-02-20 21:34:43 -0500

    doc

--- babel/lib/test.js
+++ babel/lib/test.js
@@ -1,56 +1,70 @@
 "use strict";
 
+var _obj$a, _ref;
+function _typeof(o) { "@babel/helpers - typeof"; return _typeof = "function" == typeof Symbol && "symbol" == typeof Symbol.iterator ? function (o) { return typeof o; } : function (o) { return o && "function" == typeof Symbol && o.constructor === Symbol && o !== Symbol.prototype ? "symbol" : typeof o; }, _typeof(o); }
+function _classCallCheck(a, n) { if (!(a instanceof n)) throw new TypeError("Cannot call a class as a function"); }
+function _defineProperties(e, r) { for (var t = 0; t < r.length; t++) { var o = r[t]; o.enumerable = o.enumerable || !1, o.configurable = !0, "value" in o && (o.writable = !0), Object.defineProperty(e, _toPropertyKey(o.key), o); } }
+function _createClass(e, r, t) { return r && _defineProperties(e.prototype, r), t && _defineProperties(e, t), Object.defineProperty(e, "prototype", { writable: !1 }), e; }
+function _toPropertyKey(t) { var i = _toPrimitive(t, "string"); return "symbol" == _typeof(i) ? i : i + ""; }
+function _toPrimitive(t, r) { if ("object" != _typeof(t) || !t) return t; var e = t[Symbol.toPrimitive]; if (void 0 !== e) { var i = e.call(t, r || "default"); if ("object" != _typeof(i)) return i; throw new TypeError("@@toPrimitive must return a primitive value."); } return ("string" === r ? String : Number)(t); }
 // Example JavaScript code to test Babel transpilation
 
 // Arrow functions
-const add = (a, b) => a + b;
+var add = function add(a, b) {
+  return a + b;
+};
 console.log(add(5, 3));
 
 // Template literals
-const name = "Alice";
-const greeting = `Hello, ${name}!`;
+var name = "Alice";
+var greeting = "Hello, ".concat(name, "!");
 console.log(greeting);
 
 // Classes
-class Person {
-  constructor(name) {
+var Person = /*#__PURE__*/function () {
+  function Person(name) {
+    _classCallCheck(this, Person);
     this.name = name;
   }
-  sayHello() {
-    console.log(`My name is ${this.name}`);
-  }
-}
-const person = new Person("Bob");
+  return _createClass(Person, [{
+    key: "sayHello",
+    value: function sayHello() {
+      console.log("My name is ".concat(this.name));
+    }
+  }]);
+}();
+var person = new Person("Bob");
 person.sayHello();
 
 // Spread operator
-const arr1 = [1, 2, 3];
-const arr2 = [...arr1, 4, 5];
+var arr1 = [1, 2, 3];
+var arr2 = [].concat(arr1, [4, 5]);
 console.log(arr2);
 
 // Optional chaining
-const obj = {
+var obj = {
   a: {
     b: {
       c: 42
     }
   }
 };
-const value = obj?.a?.b?.c;
+var value = obj === null || obj === void 0 || (_obj$a = obj.a) === null || _obj$a === void 0 || (_obj$a = _obj$a.b) === null || _obj$a === void 0 ? void 0 : _obj$a.c;
 console.log(value);
 
 // Nullish coalescing operator
-const nullishValue = null ?? "default";
+var nullishValue = (_ref = null) !== null && _ref !== void 0 ? _ref : "default";
 console.log(nullishValue);
 
 // const and let
-let changeable = 10;
+var changeable = 10;
 changeable = 20;
-const constant = 30;
+var constant = 30;
 console.log(changeable, constant);
 
 // for...of loop
-const iterable = [10, 20, 30];
-for (const item of iterable) {
+var iterable = [10, 20, 30];
+for (var _i = 0, _iterable = iterable; _i < _iterable.length; _i++) {
+  var item = _iterable[_i];
   console.log(item);
 }
